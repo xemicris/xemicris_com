81d13d7bdf57e2366aba0acd792783646ca42110
Chema Calavia Rivera <xemicr@gmail.com>
2024-04-10 19:42:07 +0200

    problemas en paneles técnicos arreglados

--- pixeos/application/view/footer.php
+++ pixeos/application/view/footer.php
@@ -1,62 +1,64 @@
-            <script src="<?php echo RUTA ?>js/bootstrap.bundle.min.js"></script>
-            <script type="module" src="<?php echo RUTA ?>js/getProjectRoutes.js"></script>
-            <script type="module" src="<?php echo RUTA ?>js/password.js"></script>
 
-            <?php
-                $urlActual = $_SERVER["REQUEST_URI"];
-                $url = encabezado(($urlActual));
-                if ($url == 'login' || $url == 'registration' || $url == 'recover') {
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous" fetchpriority="low"></script>
+          <script type="module" src="<?php echo RUTA ?>js/getProjectRoutes.js" fetchpriority="low" crossOrigin="anonymous"></script>
+          <script type="module" src="<?php echo RUTA ?>js/password.js" fetchpriority="low" crossOrigin="anonymous"></script>
+          <?php
+            $urlActual = $_SERVER["REQUEST_URI"];
+            $url = encabezado(($urlActual));
+            if ($url == 'login' || $url == 'registration' || $url == 'recover') {
             ?>
-                <div class="container-md" >
-                    <div class="card sombra-formulario">
-                        <ul class="politicas gap-3 m-2">
-                            <li><a href="<?php echo RUTA . '/access/legal';?>">Aviso Legal</a></li>
-                            <li><a href="<?php echo RUTA . '/access/privacy';?>">Política de Privacidad</a></li>
-                            <li><a href="<?php echo RUTA . '/access/cookies';?>">Política de Cookies</a></li>
-                            <li><a href="<?php echo XEMICRIS . 'resumenPixeos';?>">Ir a xemicris</a></li>
-                        </ul>
-                    </div>
-                </div>
-            <?php }else if ($url === 'panel' || $url === 'profile' || $url === 'project' || $url === 'viewStatistics' || $url === 'contact' || $url === 'calendar') {?>
-                <script type="module" src="<?php echo RUTA ?>js/oscuro.js"></script>
-                <script type="module" src="<?php echo RUTA ?>js/ocultarPanelSuperior.js"></script>
-            <?php } ?>
+              <div class="container-md">
+                  <div class="card sombra-formulario">
+                      <ul class="politicas gap-3 m-2">
+                          <li><a href="<?php echo RUTA . '/access/legal'; ?>">Aviso Legal</a></li>
+                          <li><a href="<?php echo RUTA . '/access/privacy'; ?>">Política de Privacidad</a></li>
+                          <li><a href="<?php echo RUTA . '/access/cookies'; ?>">Política de Cookies</a></li>
+                          <li><a href="<?php echo XEMICRIS . 'resumenPixeos'; ?>">Ir a xemicris</a></li>
+                      </ul>
+                  </div>
+              </div>
+          <?php } else if ($url === 'panel' || $url === 'profile' || $url === 'project' || $url === 'viewStatistics' || $url === 'contact' || $url === 'calendar' || $url === 'admin' || $url === 'tec') { ?>
+              <script type="module" as="script" src="<?php echo RUTA ?>js/darkMode.js" fetchpriority="low" crossOrigin="anonymous"></script>
+              <script src="//cdn.jsdelivr.net/npm/sweetalert2@11" link as="style" fetchpriority="low"></script>
+              <script type="module" src="<?php echo RUTA ?>js/ocultarPanelSuperior.js" fetchpriority="low" crossOrigin="anonymous"></script>
+          <?php } ?>
 
-            <?php
-                $urlActual = $_SERVER["REQUEST_URI"];
-                $url = encabezado(($urlActual));
-                if ($url == 'panel') {
+          <?php
+            $urlActual = $_SERVER["REQUEST_URI"];
+            $url = encabezado(($urlActual));
+            if ($url == 'panel') {
             ?>
-                <script type="module" src="<?php echo RUTA ?>js/notes.js"></script> 
-                <script type="module" src="<?php echo RUTA ?>js/searchNotes.js"></script> 
+              <script type="module" src="<?php echo RUTA ?>js/notes.js" fetchpriority="low" crossOrigin="anonymous"></script>
+              <script type="module" src="<?php echo RUTA ?>js/searchNotes.js" fetchpriority="low" crossOrigin="anonymous"></script>
 
-            <?php }else if($url == 'profile'){ ?>
+          <?php } else if ($url == 'profile') { ?>
 
-                <script src="<?php echo RUTA ?>js/profile.js"></script>  
+              <script src="<?php echo RUTA ?>js/profile.js" fetchpriority="low"></script>
 
-            <?php }else if($url == 'project'){ ?>
+          <?php } else if ($url == 'project') { ?>
 
-                <script type="module" src="<?php echo RUTA ?>js/tasks.js"></script> 
+              <script type="module" src="<?php echo RUTA ?>js/tasks.js" fetchpriority="low" crossOrigin="anonymous"></script>
 
-            <?php }else if($url == 'viewStatistics'){ ?>
+          <?php } else if ($url == 'viewStatistics') { ?>
 
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
-                <script type="module" src="<?php echo RUTA ?>js/statistics.js"></script>  
+              <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js" fetchpriority="low"></script>
+              <script type="module" src="<?php echo RUTA ?>js/statistics.js" fetchpriority="low" crossOrigin="anonymous"></script>
 
-            <?php }else if($url == 'admin'){ ?>
+          <?php } else if ($url == 'admin') { ?>
 
-                <script type="module" src="<?php echo RUTA ?>js/admin.js"></script>
+              <script type="module" src="<?php echo RUTA ?>js/admin.js" fetchpriority="low"></script>
 
-            <?php } else if($url == 'tec'){ ?>
+          <?php } else if ($url == 'tec') { ?>
 
-                <script type="module" src="<?php echo RUTA ?>js/incidencias.js"></script>
+              <script type="module" src="<?php echo RUTA ?>js/incidencias.js" fetchpriority="low"></script>
 
-            <?php } else if ($url == 'calendar'){ ?>
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-                <!--toastr-->
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-                <script type="module" src="<?php echo RUTA ?>js/calendar.js"></script>
-            <?php } ?>
-        </footer>
-    </body>
-</html>
+          <?php } else if ($url == 'calendar') { ?>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js" fetchpriority="low"></script>
+              <!--toastr-->
+              <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" fetchpriority="low"></script>
+              <script type="module" src="<?php echo RUTA ?>js/calendar.js"></script>
+          <?php } ?>
+          </footer>
+          </body>
+
+          </html>
--- pixeos/application/view/header.php
+++ pixeos/application/view/header.php
@@ -5,12 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" link as="style">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Raleway:wght@400;700;900&display=swap" as="style">
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11" link as="style"></script>
-    <link rel="stylesheet" href="<?php echo RUTA ?>css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" link as="style" fetchpriority="low">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" fetchpriority="low">
+    <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Raleway:wght@400;700;900&display=swap" as="style" fetchpriority="low">
+    <link rel="stylesheet" href="<?php echo RUTA ?>css/style.css" fetchpriority="low">
     <link rel="shortcut icon" href="<?php echo RUTA . 'public/images/logo-movil.ico'; ?>">
     <title><?php echo $datos['titulo'] ?></title>
     <meta name="theme-color" content="#F5BC51">
@@ -161,7 +159,7 @@
             <div class="container-fluid">
 
                 <a class="color-logo navbar-brand fw-bold " href="<?php echo RUTA . 'admin/admin' ?>">
-                    <img id="logo" src="<?php echo RUTA ?>public/images/logo-oscuro.png" alt="logo">
+                    <img id="logo" src="<?php echo RUTA ?>public/images/logo-oscuro.avif" alt="logo">
                 </a>
                 <button id="dde" class="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span id="boton-derecho" class="navbar-toggler-icon navbar-dark"></span>
