4a595eae9a95162c10e660bd106cc9e58f340426
Chema Calavia Rivera <xemicr@gmail.com>
2024-04-25 10:18:51 +0200

    xcppt

--- plantillas/vistas/competicion/indiceVista.php
+++ plantillas/vistas/competicion/indiceVista.php
@@ -4,16 +4,20 @@
 <head>
   <!-- etiqueta que indica la codificación del documento -->
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Security-Policy" content="script-src 'self' https://code.jquery.com/jquery-3.7.1.min.js; object-src 'self'">
+  <meta name="description" content="Juego de carreras de coches">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="shortcut icon" href="<?php echo FAVICON . "favicon.ico"; ?>">
+  <title><?php echo $datos['titulo']; ?></title>
   <!-- enlace a la hoja de estilo CSS -->
-  <link rel="stylesheet" type="text/css" href="<?php echo CSS . "competicion.css"; ?>">
-  <!-- enlace al CDN jQuery de Google para poder usar la librería jQuery -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="<?php echo CSS . "competicion.css"; ?>" fetchpriority="low">
 </head>
 <!-- cuerpo del documento -->
 <body>
+  <video src="<?php echo VIDEO . "comp.mp4"; ?>" autoplay muted></video>
    <!-- cabecera -->
    <header class="fondo__titulo">
-    <video src="<?php echo VIDEO . "competicion.mp4"; ?>" autoplay muted></video>
     <div class="texto">
       <span>C</span>
       <span>O</span>
@@ -33,64 +37,82 @@
     <!-- lista desplegable -->
     <fieldset class="borde">
       <div class="casa__contenedor">
-        <a href="<?php echo RUTABASE . "trabajos"; ?>">
+        <a href="<?php echo RUTABASE . "trabajos"; ?>" aria-label="volver a xemicris">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="casa w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
           </svg>
         </a>
       </div>
-        <div id class="seleccionar">
-          <label for="coches">Selecciona:</label>
-          <select name="coches" id="elegir">
-            <option value="0">Coches</option>
+      <div id class="seleccionar">
+        <label for="elegir">Selecciona:</label>
+        <select name="coches" id="elegir">
+          <option value="0">Coches</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
+      </div>
+      <div class="apuesta">
+        <div>
+          <label for="apostar">Elige el ganador:</label>
+          <select name="apuesta" id="apostar">
+            <option value="0">Ganador</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
           </select>
         </div>
-        <!-- participantes seleccionados -->
-        <div id="participantes"></div>
+        <div>
+          <label for="dinero">¿Qué cantidad quieres apostar?:</label>
+          <input type="number" name="dinero" id="dinero">
+        </div>
+        <p>Saldo disponible: <span id="saldo"></span></p>
+      </div>
+      <!-- participantes seleccionados -->
+      <div id="participantes"></div>
       <div class="botones">
         <!-- botón de inicio de la cocherera y para reiniciarla -->
         <button type="submit" id="iniciar">Iniciar</button>
         <button type="submit" id="reiniciar">Reiniciar</button>
       </div>
     </fieldset>
-    
-    
     <!-- imágenes de los coches -->
-    <div id="pista">
+    <div class="pista1">
       <div class="coche1">
-        <img src="<?php echo IMAGENES . "competicion/coche1.png"; ?>"  width="200px" height="93px" alt="Car 1" id="coche1" class="coche"><br>
-        <img src="<?php echo IMAGENES . "competicion/ruedaCoche11.png"; ?>" width="36px" height="36px" alt="rueda" id="ruedaCoche11" class="ruedaCoche"><br>
-        <img src="<?php echo IMAGENES . "competicion/ruedaCoche12.png"; ?>" width="36px" height="36px" alt="rueda" id="ruedaCoche12" class="ruedaCoche"><br>
+        <img src="<?php echo IMAGENES . "competicion/coche1.avif"; ?>" loading="lazy"  width="200px" height="93px" alt="Car 1" id="coche1" class="coche"><br>
+        <img src="<?php echo IMAGENES . "competicion/ruedaCoche.avif"; ?>" loading="lazy" width="36px" height="36px" alt="rueda" id="ruedaCoche11" class="ruedaCoche"><br>
+        <img src="<?php echo IMAGENES . "competicion/ruedaCoche.avif"; ?>" loading="lazy" width="36px" height="36px" alt="rueda" id="ruedaCoche12" class="ruedaCoche"><br>
       </div>
-      
-
+    </div>
+    <div class="pista2">
       <div class="coche2">
-          <img src="<?php echo IMAGENES . "competicion/coche2.png"; ?>"  width="200px" height="100px" alt="Car 2" id="coche2" class="coche"><br>
-          <img src="<?php echo IMAGENES . "competicion/ruedaCoche21.png"; ?>" width="36px" height="36px" alt="rueda" id="ruedaCoche21" class="ruedaCoche"><br>
-          <img src="<?php echo IMAGENES . "competicion/ruedaCoche22.png"; ?>" width="36px" height="36px" alt="rueda" id="ruedaCoche22" class="ruedaCoche"><br>
+          <img src="<?php echo IMAGENES . "competicion/coche2.avif"; ?>" loading="lazy"  width="200px" height="100px" alt="Car 2" id="coche2" class="coche"><br>
+          <img src="<?php echo IMAGENES . "competicion/ruedaCoche.avif"; ?>" loading="lazy" width="36px" height="36px" alt="rueda" id="ruedaCoche21" class="ruedaCoche"><br>
+          <img src="<?php echo IMAGENES . "competicion/ruedaCoche.avif"; ?>" loading="lazy" width="36px" height="36px" alt="rueda" id="ruedaCoche22" class="ruedaCoche"><br>
       </div>
-      
-      <div class="coche3">
-        <img src="<?php echo IMAGENES . "competicion/coche3.png"; ?>" width="200px" height="49px" alt="Car 3" id="coche3" class="coche"><br>
-        <img src="<?php echo IMAGENES . "competicion/ruedaCoche31.png"; ?>" width="36px" height="36px" alt="rueda" id="ruedaCoche31" class="ruedaCoche"><br>
-        <img src="<?php echo IMAGENES . "competicion/ruedaCoche32.png"; ?>" width="36px" height="36px" alt="rueda" id="ruedaCoche32" class="ruedaCoche"><br>
-      </div>
-     
-      <div class="coche4">
-        <img src="<?php echo IMAGENES . "competicion/coche4.png"; ?>" width="200px" height="100px" alt="Car 4" id="coche4" class="coche"><br>
-        <img src="<?php echo IMAGENES . "competicion/ruedaCoche41.png"; ?>" width="36px" height="36px" alt="rueda" id="ruedaCoche41" class="ruedaCoche"><br>
-        <img src="<?php echo IMAGENES . "competicion/ruedaCoche42.png"; ?>" width="36px" height="36px" alt="rueda" id="ruedaCoche42" class="ruedaCoche"><br>
-      </div>
-     
+    </div>
       <!-- resultado de la cocherera -->
         <div id="ganador"></div>
-    </div>
+      <div class="pista3">
+        <div class="coche3">
+          <img src="<?php echo IMAGENES . "competicion/coche3.avif"; ?>" loading="lazy" width="200px" height="49px" alt="Car 3" id="coche3" class="coche"><br>
+          <img src="<?php echo IMAGENES . "competicion/ruedaCoche.avif"; ?>" loading="lazy" width="36px" height="36px" alt="rueda" id="ruedaCoche31" class="ruedaCoche"><br>
+          <img src="<?php echo IMAGENES . "competicion/ruedaCoche.avif"; ?>" loading="lazy" width="36px" height="36px" alt="rueda" id="ruedaCoche32" class="ruedaCoche"><br>
+        </div>
+      </div>
+      <div class="pista4">
+        <div class="coche4">
+          <img src="<?php echo IMAGENES . "competicion/coche4.avif"; ?>" loading="lazy" width="200px" height="100px" alt="Car 4" id="coche4" class="coche"><br>
+          <img src="<?php echo IMAGENES . "competicion/ruedaCoche.avif"; ?>" loading="lazy" width="36px" height="36px" alt="rueda" id="ruedaCoche41" class="ruedaCoche"><br>
+          <img src="<?php echo IMAGENES . "competicion/ruedaCoche.avif"; ?>" loading="lazy" width="36px" height="36px" alt="rueda" id="ruedaCoche42" class="ruedaCoche"><br>
+        </div>
+      </div>
+    
   </div>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous" defer></script>
    <!-- enlace a mi script javascript -->
-   <script src="<?php echo JS . 'competicion.js'; ?>"></script>
+   <script src="<?php echo JS . 'competicion.js'; ?>" defer></script>
 </body>
 </html>
--- plantillas/vistas/piedrapapeltijera/indiceVista.php
+++ plantillas/vistas/piedrapapeltijera/indiceVista.php
@@ -2,10 +2,14 @@
 <html lang="es">
 	<head>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="preload" href="<?php echo CSS. "normalice.css"; ?>"  type="text/css">
-		<link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="<?php echo CSS. "piedrapapeltijera.css"; ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta http-equiv="Content-Security-Policy" content="script-src 'self'; object-src 'self'">
+		<meta name="description" content="Juego de piedra, papel o tijera">
+		<link rel="prefetch" href="<?php echo CSS. "normalice.css"; ?>"  type="text/css">
+		<link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet" crossorigin="anonymous" fetchpriority="low">
+		<link rel="stylesheet" type="text/css" href="<?php echo CSS. "piedrapapeltijera.css"; ?>" crossorigin="anonymous" fetchpriority="low">
+		<link rel="shortcut icon" href="<?php echo FAVICON . "favicon.ico"; ?>">
 		<title><?php echo isset($datos['titulo']) ? $datos['titulo'] : 'xemicris.com | Piedra, papel, tijera' ?></title>
 	</head>
 	<body>
@@ -14,13 +18,12 @@
 			<h1 class="titulo">Piedra-Papel-Tijera</h1>
 		</header>
 		<div class="casa__contenedor">
-			<a href="<?php echo RUTABASE . "trabajos"; ?>">
+			<a href="<?php echo RUTABASE . "trabajos"; ?>" aria-label="volver a xemicris">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="casa w-6 h-6">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
 				</svg>
 			</a>
 		</div>
-		
 		<div class="contenedor">
 			<fieldset class="formulario">
 				<legend>Datos del jugador</legend>
@@ -36,25 +39,20 @@
 					<button class="boton">CARGAR DATOS DEL JUGADOR</button>
 				</div>
 			</fieldset>
-			
 			<div class="imagenes">
-				<h3 class="partidas">Jugando la partida <span id="actual">0</span> de <span id="total">0</span>.</h3>
-
+				<h2 class="partidas">Jugando la partida <span id="actual">0</span> de <span id="total">0</span>.</h2>
 				<div id="jugador">
-					<img class="seleccionado ima" src="<?php echo IMAGENES . "piedrapapeltijera/defecto1.svg"; ?>">
-					<img class="noSeleccionado ima" src="<?php echo IMAGENES . "piedrapapeltijera/defecto1.svg"; ?>">
-					<img class="noSeleccionado ima" src="<?php echo IMAGENES . "piedrapapeltijera/defecto1.svg"; ?>">
+					<img class="seleccionado ima" src="<?php echo IMAGENES . "piedrapapeltijera/defecto1.svg"; ?>" alt="imagen inicial de la piedra">
+					<img class="noSeleccionado ima" src="<?php echo IMAGENES . "piedrapapeltijera/defecto1.svg"; ?>" alt="imagen inicial del papel">
+					<img class="noSeleccionado ima" src="<?php echo IMAGENES . "piedrapapeltijera/defecto1.svg"; ?>" alt="imagen inicial de la tijera">
 				</div>
-
-				<button class=" boton boton__jugar">¡JUGAR!</button></h2>
+				
 				<div id="maquina">
-					<img class="defecto__maquina" src="<?php echo IMAGENES . "piedrapapeltijera/robot.svg"; ?>">
+					<img class="defecto__maquina" src="<?php echo IMAGENES . "piedrapapeltijera/robot.svg"; ?>" alt="imagen por defecto de la máquina">
 				</div>
-
 			</div>
-
 			<div class="puntuaciones">
-				<h3 class="puntuaciones__titulo">Puntuaciones</h3>
+				<h2 class="puntuaciones__titulo">Puntuaciones</h2>
 				<div class="puntuaciones__script">
 					<p class="puntuacion__jugador"></p>
 					<p class="puntuacion__maquina"></p>
@@ -63,11 +61,7 @@
 				
 				<button class="boton boton__reiniciar">REINICIAR</button>
 			</div>
-
-
 		</div>
-		
-		<script type="text/javascript" src="<?php echo JS . "piedrapapeltijera.js"; ?>"></script>
-
+		<script defer type="text/javascript" src="<?php echo JS . "piedrapapeltijera.js"; ?>"></script>
 	</body>
 </html>
