5393f4f384b443572409132a8dc3c2cbdd6ac91c
Chema Calavia Rivera <xemicr@gmail.com>
2024-04-25 13:05:26 +0200

    xEp - xcd

--- plantillas/vistas/contador/indiceVista.php
+++ plantillas/vistas/contador/indiceVista.php
@@ -2,14 +2,18 @@
 <html lang="es">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="./vite.svg" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="<?php echo CSS . 'contadorDigital.css'; ?>" type="text/css">
+    <meta http-equiv="Content-Security-Policy" content="script-src 'self'; object-src 'self'">
+    <meta name="description" content="Contador de clics">
+    <link rel="icon" type="image/svg+xml" href="./vite.svg" />
+    <link rel="shortcut icon" href="<?php echo FAVICON . "favicon.ico"; ?>">
+    <link rel="stylesheet" href="<?php echo CSS . 'contadorDigital.css'; ?>" type="text/css" fetchpriority="low">
     <title>Contador Digital</title>
   </head>
   <body>
     <div class="casa__contenedor">
-          <a href="<?php echo RUTABASE . "trabajos"; ?>">
+          <a href="<?php echo RUTABASE . "trabajos"; ?>" aria-label="volver a xemicris">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="casa w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
             </svg>
